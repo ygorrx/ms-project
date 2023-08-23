4061031c04eacd6c54da979bb40abb0898ae8c9c
ygorrx <ygoroliveira55@gmail.com>
2023-08-23 16:19:18 -0300

    Rest Template

--- hr-payroll/src/main/java/com/project/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/project/hrpayroll/entities/Worker.java
@@ -2,49 +2,45 @@ package com.project.hrpayroll.entities;
 
 import java.io.Serializable;
 
-public class Payment implements Serializable {
+public class Worker implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	private Long id;
 	private String name;
 	private Long dailyIncome;
-	private Integer days;
+	 
+	public Worker() {
+		 
+	}
+	 
+	public Worker(Long id, String name, Long dailyIncome) {
+		 super();
+		 this.id = id;
+		 this.name = name;
+		 this.dailyIncome = dailyIncome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 	
-	public Payment() {
-		
-	}
-
-	public Payment(String name, Long long1, Integer days) {
-		super();
-		this.name = name;
-		this.dailyIncome = long1;
-		this.days = days;
-	}
-
 	public String getName() {
 		return name;
 	}
-
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
 	public Long getDailyIncome() {
 		return dailyIncome;
 	}
-
+	
 	public void setDailyIncome(Long dailyIncome) {
 		this.dailyIncome = dailyIncome;
-	}
-
-	public Integer getDays() {
-		return days;
-	}
-
-	public void setDays(Integer days) {
-		this.days = days;
-	}
-	
-	public double getTotal() {
-		return days*dailyIncome;
 	}
 }
