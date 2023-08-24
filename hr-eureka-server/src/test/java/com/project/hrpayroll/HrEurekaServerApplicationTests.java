f484e34288d25eb98408385efe485572293ae40e
ygorrx <ygoroliveira55@gmail.com>
2023-08-24 00:40:23 -0300

    Configuring Eureka Server

---
+++ hr-eureka-server/src/test/java/com/project/hrpayroll/HrEurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.project.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
